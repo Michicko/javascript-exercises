47b768c1020bd93272630d69bcad642d38bf6e9e
Michicko <53019881+Michicko@users.noreply.github.com>
2024-09-09 02:56:22 +0100

    completed 02_repeatString exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if(num < 0) return 'ERROR';
+    let repeatedStrings = '';
+    for(let i = 1; i <= num; i++){
+        repeatedStrings += string;
+    }
+    return repeatedStrings;
 };
 
 // Do not edit below this line
