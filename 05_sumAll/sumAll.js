275f40e272cbd0fd32e179f2a5e670c8b089d8c4
Michicko <53019881+Michicko@users.noreply.github.com>
2024-09-09 03:53:22 +0100

    completed sumAll exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function(startInteger, endInteger) {
+    if(
+        startInteger < 0 || endInteger < 0 ||
+        !Number.isInteger(startInteger) || !Number.isInteger(endInteger)
+    ) return 'ERROR';
+    
+    let start = startInteger;
+    let end = endInteger;
+    let result = 0;
 
+    if(startInteger > endInteger){
+        start = endInteger;
+        end = startInteger;
+    }
+
+    for(let i = start; i <= end; i++){
+        result += i;
+    }
+
+    return result;
 };
 
 // Do not edit below this line
