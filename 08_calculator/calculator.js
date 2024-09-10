a1c015fa49a00a1f69b5e4e1fceac103fb886aa9
Michicko <53019881+Michicko@users.noreply.github.com>
2024-09-10 22:18:01 +0100

    compledted calculator exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,43 @@
-const add = function() {
-	
+const add = function(...args) {
+	const total = args.reduce((acc, current) => acc + current);
+  return total;
 };
 
-const subtract = function() {
-	
+const subtract = function(...args) {
+	const total = args.reduce((acc, current) => acc - current);
+  return total;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	let total = 0;
+  for(let i = 0; i < array.length; i++){
+    total += array[i];
+  }
+  return total;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+	let total = 1;
+  for(let i = 0; i < array.length; i++){
+    total *= array[i];
+  }
+  return total;
 };
 
-const power = function() {
-	
+const power = function(base, power) {
+	let result = 1;
+  for(let i = 1; i <= power; i++){
+    result *= base;
+  }
+  return result;
 };
 
-const factorial = function() {
-	
+const factorial = function(factor) {
+	let total = 1;
+  for(let i = factor; i >= 1; i--){
+    total *= i;
+  }
+  return total;
 };
 
 // Do not edit below this line
