eab2b1ab9d7d341b74b482c896323534f1d85141
Michicko <53019881+Michicko@users.noreply.github.com>
2024-09-10 23:40:08 +0100

    completed palindromes exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    // remove space and comma from string
+    const bareStr = str.replace(/[^a-zA-Z0-9]/g, '').trim().toLowerCase();
+    const reseredArrStr = bareStr.split('').reverse();
+    const reversedStr = reseredArrStr.join('');
+    return bareStr === reversedStr;
 };
 
 // Do not edit below this line
