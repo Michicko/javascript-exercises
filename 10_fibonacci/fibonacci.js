b2a5a52d7937417c8a46d953af4ea3f32be035c3
Michicko <53019881+Michicko@users.noreply.github.com>
2024-09-11 05:45:42 +0100

    completed the 10_fibonacci exercise
    - implemented the function to throw an error
      for negatives
    - implemented the feature to accept strings

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    const fib = [0, 1];
+    n = +n;
+    if(n < 0) return 'OOPS'
+    if(n == 0) return fib[0];
+    if(n == 1) return fib[1];
+    
+    for(let i = 2; i <= n; i++){
+        // formula for fibonacci sequence
+        let fibI = fib[i-1] + fib[i-2];
+        fib.push(fibI);
+    }
 
+    return fib[fib.length - 1];
 };
 
 // Do not edit below this line
