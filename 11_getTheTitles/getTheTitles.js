c4aac29a08a2027b9fd080968600785c5bedbed3
Michicko <53019881+Michicko@users.noreply.github.com>
2024-09-11 05:49:42 +0100

    completed the 11_getTheTitles exercise
    Applied the array map method to resturn a
    transformed books array showing only the title

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,6 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    const transformedBooks = books.map((book) => book.title);
+    return transformedBooks;
 };
 
 // Do not edit below this line
